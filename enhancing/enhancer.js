4e08bd670450f7062091db88f871fd5728cd098e
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-08 15:21:00 -0400

    Initial commit. Added jest. Wrote functions! Onto testing!

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,20 +1,42 @@
+// a repair(item) method that accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
+// a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
+// a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
+// a get() method for use when working on the stretch problem.
+
+// item = {
+//   name: '',
+//   durability: '',
+//   enhancement: '',
+// }
+
 module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  const newItem =
+    item.enhancement === 20 ? item.enhancement : item.enhancement++;
+  return { ...newItem };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability - 5;
+  } else {
+    if (item.enhancement === 15) {
+      item.durability - 10;
+    } else {
+      item.durability - 10 && item.enhancement - 1;
+    }
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,26 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair } = require("./enhancer.js");
 // test away!
+
+const magicSword = {
+  name: "Magic Sword",
+  durability: 5,
+  enhancement: 6
+};
+
+const ironSword = {
+  name: "Iron Sword",
+  durability: 25,
+  enhancement: 15
+};
+
+const fireSword = {
+  name: "Fire Sword",
+  durability: 50,
+  enhancement: 20
+};
+
+const rustySword = {
+  name: "Rusty Sword",
+  durability: 5,
+  enhancement: 16
+};
